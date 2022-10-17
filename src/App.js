512b870e25a0294904d8a4c991e6bbb3dfb9984f
gaaveloso <gabrielvelosorezende-po@hotmail.com>
2022-10-17 16:31:42 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import Garagem from "./Componentes/Garagem";
 import { GlobalStyled } from "./globalStyled";
+import { useState } from "react";
 
 export default function App() {
   const nome = "Turma";
+  const [nomeEstado,setNomeEstado] = useState("Turma Ammal")
+
   function apresentaGaragem() {
     alert(`Boas vindas à garagem de ${nome}`);
   }
 
+  const mudarNome = () => {
+    setNomeEstado("Turma mais divertida!")
+  }
   return (
     <div className="App">
       <GlobalStyled />
 
-      <Garagem nome={nome} mensagemAprentacao={apresentaGaragem} />
+      <button onClick={mudarNome}>atualizar nome</button>
+
+      <Garagem
+      nome={nomeEstado}
+      mudarNome = {mudarNome}
+      mensagemAprentacao={apresentaGaragem}
+      />
     </div>
   );
 }
--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -1,22 +1,45 @@
 import Carro from "./Carro";
 import { Button, EstilizacaoDaGaragem } from "./styledGaragem";
+import {useState} from 'react'
 
 function Garagem(props) {
+
+  const [automovel1, setAutomovel1] = useState({
+    cor: "azul",
+    ano: 2030,
+    flex: "as vezes"
+  })
+
+  const [automovel2, setAutomovel2] = useState({
+    cor: "verde",
+    ano: 2050,
+    flex: "sempre"
+  })
+
+  const mudarAutomovel1 = () => {
+    setAutomovel1({
+      cor: "rosa",
+      ano: "2031",
+      flex: "sempre"
+    })
+  } 
+
   return (
     <EstilizacaoDaGaragem>
       <h1>Garagem da {props.nome}</h1>
       <Button onClick={props.mensagemAprentacao}>Mensagem</Button>
       <Carro
         adicionadoPor={props.nome}
-        cor={"Vermelho"}
-        ano={2022}
-        flex={"true"}
+        cor={automovel1.cor}
+        ano={automovel1.ano}
+        flex={automovel1.flex}
       />
+      <Button onClick={mudarAutomovel1}>Mudar Carro</Button>
       <Carro
         adicionadoPor={props.nome}
-        cor={"Verde"}
-        ano={2020}
-        flex={"false"}
+        cor={automovel2.cor}
+        ano={automovel2.ano}
+        flex={automovel2.flex}
       />
       <Carro
         adicionadoPor={props.nome}
@@ -24,7 +47,12 @@ function Garagem(props) {
         ano={2009}
         flex={"false"}
       />
-      <Carro adicionadoPor={props.nome} cor={"Rosa"} ano={2022} flex={"true"} />
+      <Carro 
+        adicionadoPor={props.nome}
+        cor={"Rosa"}
+        ano={2022}
+        flex={"true"}
+      />
     </EstilizacaoDaGaragem>
   );
 }
